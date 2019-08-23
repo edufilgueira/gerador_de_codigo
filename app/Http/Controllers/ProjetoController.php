d825459997a084a618553ae26c71c9507dc453b6
edufilgueira <edufilgueira@gmail.com>
2019-08-23 13:23:40 -0300

    Gerando os arquivos

--- app/Http/Controllers/ProjetoController.php
+++ app/Http/Controllers/ProjetoController.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\AppBaseController;
 use Illuminate\Http\Request;
 use App\Models\Modelo;
 use App\Models\Campo;
+use App\Models\Projeto;
 use Flash;
 use Response;
 
@@ -25,6 +26,16 @@ class ProjetoController extends AppBaseController
     public function gerar($id){
         
         $projeto_id = $id;
+        $projeto = Projeto::find($id);
+
+        //Criar estrutura de pastas
+        $this->criar_pasta($projeto->nome);
+        $this->criar_pasta($projeto->nome."/src");
+        $this->criar_pasta($projeto->nome."/src/app");
+        $caminho_model = $projeto->nome."/src/app/Models";
+        $this->criar_pasta($caminho_model);
+        $caminho_services = $projeto->nome."/src/app/services";
+        $this->criar_pasta($caminho_services);
 
         $modelos = Modelo::where('projeto_id', $projeto_id)->get();
         
@@ -33,21 +44,170 @@ class ProjetoController extends AppBaseController
             
             //Percorrer todos os campos de um modelo
             $campos = Campo::where('modelo_id', $modelo->id)->get();
-            foreach($campos as $key => $campo){
-                //dd($campo);
-            }
+            $this->gerar_model($projeto, $modelo, $campos, $caminho_model);
+            $this->gerar_service($modelo, $caminho_services);
+
         }
-        $this->gerar_arquivo("conteudo","pastas","arquivo.txt");
-        //return redirect(route('projetos.index'));
+        
+        //$this->gerar_arquivo("conteudo",$pasta,"arquivo.txt");
+        
+        //$this->delTree('pastas');
+        return redirect(route('projetos.index'));
     }
     
     private function gerar_arquivo($conteudo, $diretorio, $arquivo){
-        if(!is_dir($diretorio))
-            mkdir($diretorio);
+        $this->criar_pasta($diretorio);
         $arquivo = fopen("$diretorio/$arquivo", 'w');
         fwrite($arquivo, $conteudo);
         fclose($arquivo);
     }
+
+    private function delTree($dir) { 
+        $files = array_diff(scandir($dir), array('.','..')); 
+        foreach ($files as $file) { 
+            (is_dir("$dir/$file")) ? delTree("$dir/$file") : unlink("$dir/$file"); 
+        } 
+        return rmdir($dir); 
+    }
+
+    private function criar_pasta($diretorio){
+        if(!is_dir($diretorio))
+            mkdir($diretorio);
+    }
+
+    public function gerar_model($projeto, $modelo, $campos, $caminho){
+        $conteudo = "export class ".$modelo->singular."{
+  public id: BigInteger;\n";
+
+        foreach($campos as $key => $campo){
+            $conteudo .= "  public ".$campo->nome.": string;\n";
+            
+        }
+
+        $conteudo .="  public created_at: string;
+  public updated_at: string;
+  public deleted_at: string;
+
+  //Botão para controle interno
+  public display_button = false;
+
+  constructor(
+  ){}
+}";
+
+        $this->gerar_arquivo($conteudo,$caminho,$modelo->singular.".ts");
+
+    }
+
+    public function gerar_service($modelo, $caminho){
+        $this->criar_pasta($caminho."/".$modelo->singular);
+        $conteudo = "import { AgeRange } from '../../models/age_range';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
+//ADD MANUAL
+import { VariablesService } from './../../services/variables.service';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Accept': 'application/json',
+    'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImQzZmYxODIxNDNkOGIzODQ4ZjIzMzQ1YmNiMDc2NjUzMTE0Y2JmMTRlM2M4Nzk0OGFkMzQ0NGYxYWU3MTM5MjRiMjdjMDU1YmM4NDYxMGZmIn0.eyJhdWQiOiIxIiwianRpIjoiZDNmZjE4MjE0M2Q4YjM4NDhmMjMzNDViY2IwNzY2NTMxMTRjYmYxNGUzYzg3OTQ4YWQzNDQ0ZjFhZTcxMzkyNGIyN2MwNTViYzg0NjEwZmYiLCJpYXQiOjE1NTkxODgyMjAsIm5iZiI6MTU1OTE4ODIyMCwiZXhwIjoxNTkwODEwNjIwLCJzdWIiOiIyIiwic2NvcGVzIjpbXX0.B0CIQBK98xikoIAq28JjEosFRvIxaZ8vGtUhrK5SKeU-b0RuAKcfXzQzZmPDyg_CyYOb1KF7VQ0odxK5cIw9wXmRSGMNK-Gdo7sqqN5SZKgd-rIm91gukqbHnxj_rFoCjBuRgblMgvEvlq22IuCT3n0jvSGWhcq3NXfU7-O0V56LPg9ShcQ41U3HkY5LOdDsDM-GRHCI0fiYZ9rFjmRfGZd0PFyvuaNzfd6oEL9eTTcgZBTA8gBi2b9UwDoSA55q0ly8g1Ph9RLoBGLuWpKVwACJ-rfMenVe4UjckCmDCcxn3x37tLe74RDUnjhM3e__uOHCjdLDwmplrd9qhN5S9E3ZAITMyiDVfC8w2840QNLmCKYqyUM23ctLf0nKLoarDteRpS8teoayeJOkzOt4NTXJ3-2IwWS1iemq6JqAqpU07Zy0ig9VTCoL0mPjjDmHSzrQyanUHmGVhF-0HVvNhLGjxyKI65GtVrPKeFbQTj4vIx42fV6bLyuSfT33SUTFR_UUwjSetITpLiRfnQ_wRbVGJuhnEF8382Hg012uXzSzTMDnRcVXxZdtphRApCnH_i6bI6O5Oek2m1NyinlkNAW8jlbX1Bhp013gtcALLAWYPS1JvRezj_u5vS-Gt_p5MGhD56omAgWuPD1AXfrnfeRYwHIssv7-PUp6HdqZwpA'})
+};
+
+const head= new HttpHeaders() .append('accept', 'application/json') .append('Authorization', 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImQzZmYxODIxNDNkOGIzODQ4ZjIzMzQ1YmNiMDc2NjUzMTE0Y2JmMTRlM2M4Nzk0OGFkMzQ0NGYxYWU3MTM5MjRiMjdjMDU1YmM4NDYxMGZmIn0.eyJhdWQiOiIxIiwianRpIjoiZDNmZjE4MjE0M2Q4YjM4NDhmMjMzNDViY2IwNzY2NTMxMTRjYmYxNGUzYzg3OTQ4YWQzNDQ0ZjFhZTcxMzkyNGIyN2MwNTViYzg0NjEwZmYiLCJpYXQiOjE1NTkxODgyMjAsIm5iZiI6MTU1OTE4ODIyMCwiZXhwIjoxNTkwODEwNjIwLCJzdWIiOiIyIiwic2NvcGVzIjpbXX0.B0CIQBK98xikoIAq28JjEosFRvIxaZ8vGtUhrK5SKeU-b0RuAKcfXzQzZmPDyg_CyYOb1KF7VQ0odxK5cIw9wXmRSGMNK-Gdo7sqqN5SZKgd-rIm91gukqbHnxj_rFoCjBuRgblMgvEvlq22IuCT3n0jvSGWhcq3NXfU7-O0V56LPg9ShcQ41U3HkY5LOdDsDM-GRHCI0fiYZ9rFjmRfGZd0PFyvuaNzfd6oEL9eTTcgZBTA8gBi2b9UwDoSA55q0ly8g1Ph9RLoBGLuWpKVwACJ-rfMenVe4UjckCmDCcxn3x37tLe74RDUnjhM3e__uOHCjdLDwmplrd9qhN5S9E3ZAITMyiDVfC8w2840QNLmCKYqyUM23ctLf0nKLoarDteRpS8teoayeJOkzOt4NTXJ3-2IwWS1iemq6JqAqpU07Zy0ig9VTCoL0mPjjDmHSzrQyanUHmGVhF-0HVvNhLGjxyKI65GtVrPKeFbQTj4vIx42fV6bLyuSfT33SUTFR_UUwjSetITpLiRfnQ_wRbVGJuhnEF8382Hg012uXzSzTMDnRcVXxZdtphRApCnH_i6bI6O5Oek2m1NyinlkNAW8jlbX1Bhp013gtcALLAWYPS1JvRezj_u5vS-Gt_p5MGhD56omAgWuPD1AXfrnfeRYwHIssv7-PUp6HdqZwpA');
+
+const apiUrl = VariablesService.getUrlServer()+'age_ranges';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AgeRangeService {
+
+  constructor(
+    private httpClient: HttpClient
+    ) { }
+  
+  index(next_page_url): Observable<AgeRange[]> {
+    var url = (next_page_url) ? next_page_url:apiUrl;
+    return this.httpClient.get<AgeRange[]>(url,{headers:head})
+      .pipe(
+        tap(data => console.log('fetched AgeRange')),
+        catchError(this.handleError('AgeRange', []))
+      );
+  }
+
+  search(field, value, next_page_url): Observable<AgeRange[]> {
+    var url = (next_page_url) ? next_page_url+\`${field}=*${value}*`:`${apiUrl}?${field}=*${value}*`;
+    return this.httpClient.get<AgeRange[]>(url)
+      .pipe(
+        tap(data => console.log('api search AgeRange')),
+        catchError(this.handleError('search AgeRange', []))
+      );
+  }
+
+  show(id): Observable<AgeRange> {
+    const url = `${apiUrl}/${id}`;
+    console.log('testendo.... '+url);
+    return this.httpClient.get<AgeRange>(url).pipe(
+      tap(_ => this.log(`fetched AgeRange id=${id}`)),
+      catchError(this.handleError<AgeRange>(`AgeRange id=${id}`))
+    );
+  }
+
+  create(data): Observable<any> {
+    return this.httpClient.post(apiUrl, data, httpOptions)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  update(id, ageRange): Observable<any> {
+    const url = `${apiUrl}/${id}`;
+    return this.httpClient.put(url, ageRange, httpOptions).pipe(
+      tap(_ => this.log(`updated AgeRange id=${id}`)),
+      catchError(this.handleError<any>('AgeRange'))
+    );
+  }
+
+  destroy(id): Observable<AgeRange> {
+    const url = `${apiUrl}/${id}`;
+    return this.httpClient.delete<AgeRange>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted AgeRange`)),
+      catchError(this.handleError<AgeRange>('AgeRange'))
+    );
+  }
+
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  private log(message: string) {
+     console.log(`Service: ${message}`);
+  }
+}";
+        $this->gerar_arquivo($conteudo, $caminho."/".$modelo->singular, $modelo->singular.".service.ts");
+
+    }
+
 
     /**
      * Display a listing of the Projeto.
@@ -182,4 +342,5 @@ class ProjetoController extends AppBaseController
 
         return redirect(route('projetos.index'));
     }
+
 }
